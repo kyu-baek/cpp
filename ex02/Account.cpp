a0b6b827c6db44a20abc8f24cf6d457e458ac8ba
kyu-baek <rbfla6334@gmail.com>
2022-04-07 22:58:40 +0900

    UPDATE : ex01 done

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: kbaek <kbaek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:57:17 by kbaek             #+#    #+#             */
-/*   Updated: 2022/04/07 19:26:49 by kbaek            ###   ########.fr       */
+/*   Updated: 2022/04/07 22:55:06 by kbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,48 @@
 #include <iostream>
 #include <ctime>
 
-int	Account::getNbAccounts(void)
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts( void )
 {
 	return (_nbAccounts);
 }
 
-int	Account::getTotalAmount(void)
+int	Account::getTotalAmount( void )
 {
-	return (_totalAmount);
+	return (Account::_totalAmount);
 }
 
-int	Account::getNbDeposits(void)
+int	Account::getNbDeposits( void )
 {
-	return (_totalNbDeposits);
+	return (Account::_totalNbDeposits);
 }
 
-int	Account::getNbWithdrawals(void)
+int	Account::getNbWithdrawals( void )
 {
-	return (_totalNbWithdrawals);
+	return (Account::_totalNbWithdrawals);
 }
 
-/*
-strftime()함수는 입력받은 time_t 구조체의 정보를 지정한 양식으로 문자열로 변환시켜준다.
-[년월일_시분초] = %Y%m%d_%H%M%S
-time 함수가 돌려주는 time_t의 값은 시스템에 따라 32비트나 64비트 정수이다.
-이 값을 다음과 같이 localtime 함수를 이용해 사용하기 쉬운 형태로 변환한다.
-*/
-void Account::_displayTimestamp(void)
+void	Account::displayAccountsInfos( void )
 {
-	char	buff[16];
-	time_t	curtime = time(NULL);
-
-	strftime(buff, sizeof(buff), "%Y%m%d_%H%M%S", localtime(&curtime));
-	std::cout << "[" << buff << "]";
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
-void	Account::displayAccountsInfos(void)
-{
-	_displayTimestamp();
-	std::cout << " accounts:" << _nbAccounts;
-	std::cout << ";total:" << getTotalAmount();
-	std::cout << ";deposits:" << getNbDeposits();
-	std::cout << ";withdrawals:" << getNbWithdrawals() << std::endl;
-}
-void	Account::displayStatus(void) const
-{
-	_displayTimestamp();
-	std::cout << " index:" << _accountIndex;
-	std::cout << ";amount:" << _amount;
-	std::cout << ";deposits:" << _nbDeposits;
-	std::cout << ";withdrawals:" << _nbWithdrawals << std::endl;
-}
-Account::Account(int initial_deposit) 
-	:
+Account::Account(int initial_deposit) :
 	_accountIndex(Account::_nbAccounts++),
 	_amount(initial_deposit),
 	_nbDeposits(0),
 	_nbWithdrawals(0)
 {
 	Account::_displayTimestamp();
-	std::cout << "index:" << _accountIndex << ";";
+	std::cout << " index:" << _accountIndex << ";";
 	std::cout << "amount:" << _amount << ";";
 	std::cout << "created" << std::endl;
 	Account::_totalAmount += initial_deposit;
@@ -83,9 +65,76 @@ Account::Account(int initial_deposit)
 Account::~Account( void )
 {
 	Account::_displayTimestamp();
-	std::cout << " index:" << _accountIndex;
-	std::cout << ";amount:" << _amount;
-	std::cout << ";closed" << std::endl;
+	std::cout << " index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+/*
+strftime()함수는 입력받은 time_t 구조체의 정보를 지정한 양식으로 문자열로 변환시켜준다.
+[년월일_시분초] = %Y%m%d_%H%M%S
+time 함수가 돌려주는 time_t의 값은 시스템에 따라 32비트나 64비트 정수이다.
+이 값을 다음과 같이 localtime 함수를 이용해 사용하기 쉬운 형태로 변환한다.
+*/
+void	Account::makeDeposit(int deposit)
+{
+	Account::_amount += deposit;
+	Account::_totalAmount += deposit;
+	Account::_nbDeposits += 1;
+	Account::_totalNbDeposits += 1;
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount - deposit << ";";
+	std::cout << "deposit:" << deposit << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_deposits:" << _nbDeposits << std::endl;
+	
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	_amount -= withdrawal;
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount + withdrawal << ";";
+	if (!checkAmount())
+	{
+		std::cout << "withdrawal:refused" << std::endl;
+		_amount += withdrawal;
+		return (false);
+	}
+	_nbWithdrawals += 1;
+	std::cout << "withdrawal:" << withdrawal << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+	Account::_totalAmount -= withdrawal;
+	Account::_totalNbWithdrawals++;
+	return (true);
+}
+
+int	Account::checkAmount( void ) const
+{
+	if (_amount < 0)
+		return (0);
+	return (1);
+}
+
+void	Account::displayStatus(void) const
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "deposits:" << _nbDeposits << ";";
+	std::cout << "withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+	char	buff[16];
+	time_t	curtime = time(NULL);
+
+	strftime(buff, sizeof(buff), "%Y%m%d_%H%M%S", localtime(&curtime));
+	std::cout << "[" << buff << "]";
 }
 
 Account::Account(void){}

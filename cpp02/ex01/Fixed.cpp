9ba591e86698527e677da57cada1ef2b1a28c622
kyu-baek <rbfla6334@gmail.com>
2022-04-11 22:01:00 +0900

    ADD : new file for cpp 02

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbaek <kbaek@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 17:53:54 by kbaek             #+#    #+#             */
+/*   Updated: 2022/04/10 18:21:57 by kbaek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(void) : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int fix) : _value(fix << _bits)
+{
+	std::cout << "Int constructor called" <<std::endl;
+}
+
+Fixed::Fixed(const float fix) : _value(roundf(fix * (1 << _bits)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& fix)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fix;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed &fix)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_value = fix._value;
+	return (*this);
+}
+
+int		Fixed::toInt(void) const
+{
+	return (_value >> _bits);
+}
+float	Fixed::toFloat( void ) const
+{
+	return ((float(_value) / 256));
+}
+
+std::ostream&	operator<<(std::ostream& output, const Fixed &fix)
+{
+	output << fix.toFloat();
+	return (output);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbaek <kbaek@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 17:41:19 by kbaek             #+#    #+#             */
+/*   Updated: 2022/04/10 18:22:25 by kbaek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		FIXED_HPP
+# define	FIXED_HPP
+# include <cmath>
+# include <iostream>
+
+class Fixed {
+public:
+	Fixed(void);
+	Fixed(const int fix);
+	Fixed(const float fix);
+	Fixed(const Fixed& fix);
+	~Fixed(void);
+	Fixed& operator=(const Fixed &fix);
+	
+	int		toInt(void) const;
+	float	toFloat( void ) const;
+
+private:
+	int	_value;
+	static const int	_bits = 8;
+};
+
+std::ostream& operator<<(std::ostream& output, const Fixed &fix);
+
+#endif

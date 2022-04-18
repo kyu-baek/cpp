a11c066d07189ec683847035cfdc9f0e85e88a43
kyu-baek <rbfla6334@gmail.com>
2022-04-18 17:52:57 +0900

    UPDATE : main.cpp

--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kbaek <kbaek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 17:06:46 by kbaek             #+#    #+#             */
-/*   Updated: 2022/04/12 16:36:24 by kbaek            ###   ########.fr       */
+/*   Updated: 2022/04/18 17:47:55 by kbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int	main( void ) {
 
-	Fixed			a;
-	
+	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
+
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	
+
 	std::cout << b << std::endl;
-	
+
 	std::cout << Fixed::max( a, b ) << std::endl;
 	
 	return 0;
